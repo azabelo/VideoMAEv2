3b926139d61cf45a5010dfa65fb1767716c6cfa2
azabelo <azabelo21old@gmail.com>
2023-12-12 21:12:55 -0500

    first

--- pretrain.sh
+++ pretrain.sh
@@ -20,7 +20,7 @@ SRUN_ARGS=${SRUN_ARGS:-""}  # Other slurm task args
 PY_ARGS=${@:2}  # Other training args
 
 # Please refer to `run_mae_pretraining.py` for the meaning of the following hyperreferences
-srun -p $PARTITION \
+python3 run_mae_pretraining \
         --job-name=${JOB_NAME} \
         --gres=gpu:${GPUS_PER_NODE} \
         --ntasks=${GPUS} \
